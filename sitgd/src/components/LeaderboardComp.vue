5662f941e547ec56ec14e435249ab11e272c1ba7
dannywchen <155599374+dannywchen@users.noreply.github.com>
2024-01-10 10:56:13 -0500

    Update LeaderboardComp.vue

--- sitgd/src/components/LeaderboardComp.vue
+++ sitgd/src/components/LeaderboardComp.vue
@@ -444,3 +444,33 @@ p {
   }
 }
 </style>
+
+<template>
+  <div class="container">
+    <h2>Blog</h2>
+
+    <br />
+
+    <h3>New release of SITHS GAME DEV website</h3>
+    <p>Published on: January 11, 2024</p>
+    <br />
+    <p>
+      Exciting news! We've launched our new and improved SITHS GAME DEV website.
+      Explore the features and let us know your feedback. Website is still being
+      developed and is constantly adding new features. If you want anything to
+      be added, fill this:
+      <a href="https://forms.gle/nAT2tGQ69Db7i9KYA">Form</a>
+    </p>
+
+    <br />
+
+    <h3>Introduction to Unreal Engine 5</h3>
+    <p>Published on:January 10, 2024</p>
+    <br />
+    <p>
+      Learn about the powerful features and capabilities of Unreal Engine 5.
+      Dive into the world of game development with cutting-edge technology.
+      <a href="">Read more...</a>
+    </p>
+  </div>
+</template>
