99eb833e868c697651aeaac48b8d9265c10e0e2f
Danny Chen <155599374+dannywchen@users.noreply.github.com>
2024-01-30 01:08:09 -0500

    Update LeaderboardView.vue

--- sitgd/src/views/LeaderboardView.vue
+++ sitgd/src/views/LeaderboardView.vue
@@ -13,7 +13,7 @@ main {
   padding: 10rem 0 70px 0;
   display: flex;
   justify-content: center;
-  background: url("https://cdn2.unrealengine.com/darkworld-4k-3840x2160-2ed69bbba703.jpg?resize=1&w=1920");
+  background: url("https://cdn.discordapp.com/attachments/1164387540850188348/1201770593087922186/DALLE_2024-01-30_01.07.08_-_Create_a_4K_resolution_background_image_for_a_game_development_website._The_scene_should_be_a_beautiful_grand_and_heavenly_anime-style_landscape._Th.png?ex=65cb0715&is=65b89215&hm=bb5757f170e0b2cee6ff03eaa01092e6c1fd14f0460892a4eae072eee9b3d5f6&");
   background-position: center;
   background-size: cover;
   font: normal;
