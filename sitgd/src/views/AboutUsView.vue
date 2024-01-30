fa42697302018ab2a7a118664c7d151dca2095b9
Danny Chen <155599374+dannywchen@users.noreply.github.com>
2024-01-30 01:00:55 -0500

    Update AboutUsView.vue

--- sitgd/src/views/AboutUsView.vue
+++ sitgd/src/views/AboutUsView.vue
@@ -9,7 +9,7 @@ import ClubOfficers from "../components/ClubOfficers.vue";
 <style scoped>
 main {
   padding: 10rem 0 70px 0;
-  background: url("https://cdn2.unrealengine.com/darkworld-4k-3840x2160-2ed69bbba703.jpg?resize=1&w=1920");
+  background: url("https://cdn.discordapp.com/attachments/1164387540850188348/1201768653310402610/DALLE_2024-01-30_00.59.23_-_Create_a_4K_resolution_background_image_for_a_game_development_website_with_a_beautiful_anime-style_scenery_optimized_for_text_visibility._The_image_.png?ex=65cb0546&is=65b89046&hm=fffbb4ab6139bb181d933d8147021b59637b68b25967e8653b37f70eda45c685&");
   background-size: cover;
 }
 </style>
